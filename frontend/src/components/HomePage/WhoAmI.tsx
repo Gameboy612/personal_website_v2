f74363002418dec86c6018f4b54c92d9d89bdcc6
Gameboy612 <lokapakki@gmail.com>
2026-08-20 19:38:23 +0800

    feat(whoami): changed layout

--- frontend/src/components/HomePage/WhoAmI.tsx
+++ frontend/src/components/HomePage/WhoAmI.tsx
@@ -5,16 +5,16 @@ export default function WhoAmI() {
   return (
     <div className="w-full h-[100vh] px-10 flex flex-col items-center justify-center">
         <div className="text-2xl font-bold font-mono mb-4"><span className="text-primary">$</span> <TypingAnimation text="whoami" speedMs={40} /></div>
-        <div className="text-lg font-thin">Kapakki, <strong>B.Eng. in AI: System and Technology</strong> in The Chinese University of Hong Kong (CUHK).</div>
+        <div className="text-lg font-thin">I am Kapakki, I code and I create.</div>
         <div></div>
 
-        
+{/*         
         <div className="w-full sm:w-3/4 md:w-1/2 lg:w-1/3 mt-6">
           <ImageGallery images={[
             "",
             "",
         ]} />
-        </div>
+        </div> */}
         
 
     </div>
